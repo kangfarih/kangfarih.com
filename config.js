9b0fc5ab61553f914bd038ed5e0710a6d5c57de7
Farih Muhammad <farihul.ibad@gmail.com>
2018-10-07 06:15:13 +0700

    master

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'the-code-journey',
+		wp_ws_url : 'https://kangfarih.com/wp-appkit-api/the-code-journey',
+		wp_url : 'https://kangfarih.com',
+		theme : 'q-android',
+		version : '1.0.0',
+		app_type : 'phonegap-build',
+		app_title : 'The Code Journey',
+		app_platform : 'android',
+		app_path: '',
+		gmt_offset : 7,
+		debug_mode : 'off',
+		auth_key : 'PTL/)Mie(u.<f69fa(#-m5PV:lR/iWYWXe,9qDwPX{tT0dbml$rpy>._RR:dxTOr',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
